238b2dad4113e3ecc540e4f03682d565f4ae5f99
bony075 <bony.075@gmail.com>
2023-06-01 20:12:29 +0600

    husky test

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@
 // import app from "./app";
 // import config from "./config/index";
 
-
-
 // async function bootstrap() {
 //     try {
 //         await mongoose.connect(config.database_url as string);
@@ -13,25 +11,25 @@
 // })
 //     } catch (error) {
 //         console.log("failed to connect BD",error);
-        
+
 //     }
 // }
 // bootstrap();
-import mongoose from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`🛢   Database is connected successfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`🛢   Database is connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`);
-    });
+      console.log(`Application  listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log("Failed to connect database", err);
+    console.log('Failed to connect database', err)
   }
 }
 
-boostrap();
+boostrap()
